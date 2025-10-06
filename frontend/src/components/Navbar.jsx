fec2fb768655bf3ec6cc312e39b69d4550746110
Akshada Kadam <akshada.kadam088@gmail.com>
2025-10-06 17:53:54 +0530

    .

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -19,19 +19,19 @@ export default function Navbar() {
           <div className="hidden md:flex space-x-12">
             <a
               href="#"
-              className="text-[#000000] font-medium hover:text-[#000000] transition-colors"
+              className="text-[#000000] font-medium hover:text-[#540962] transition-colors"
             >
               Events
             </a>
             <a
               href="#"
-              className="text-[#000000] font-medium hover:text-[#000000] transition-colors"
+              className="text-[#000000] font-medium hover:text-[#540962] transition-colors"
             >
               Packs
             </a>
             <a
               href="#"
-              className="text-[#000000] font-medium hover:text-[#000000] transition-colors"
+              className="text-[#000000] font-medium hover:text-[#540962] transition-colors"
             >
               Showcase
             </a>
